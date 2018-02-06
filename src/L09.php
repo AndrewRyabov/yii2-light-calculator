ef71f701d031c500daba687c7b3ea3f43b591f45
Andrii <>
2018-02-06 22:04:22 +0200

    Переделал L09 (готовый вариант)

--- src/L09.php
+++ src/L09.php
@@ -3,9 +3,9 @@
 /**
  * ИСХОДНЫЕ ДАННЫЕ.
  * Created by PhpStorm.
- * User: Andrii
- * Date: 06.06.2017
- * Time: 23:55
+ * User: Andrew
+ * Date: 06.02.2018
+ * Time: 22:03
  *
  */
 
@@ -15,12 +15,11 @@ class L09
     public $B4_C_light; // C_light
     public $B5_S_Light; // S_light
 
-    // C_light
-    public $J4_RoofVisorOut; // крыша/козырек улица
-    public $J5_WallOut; // стена улица
-    public $J6_WallIn; // стена помещение
-    public $J7_2SideIn; // 2 стороны помещение
-    public $J8_4SideIn; // 4 стороны помещение
+    // C_light_2
+
+
+    // шаг  0
+    public $J4_VarIspoln; // вариант исполнения
 
     // шаг  1
     public $J21_Orientation; // ориентация
@@ -30,46 +29,55 @@ class L09
     public $J25_ColorSide; // цвет бортов
     public $J26_ColorBack; // цвет тыла
 
+    public $J29_LevVerhUgolRad; // левый верхний угол радиус (0/6/12), см
+    public $J30_PravVerhUgolRad; // правый верхний угол радиус (0/6/12), см
+    public $J31_PravNijnUgolRad; // правый нижний угол радиус (0/6/12), см
+    public $J32_LevNijnUgolRad; // левый нижний угол радиус (0/6/12), см
+
     // шаг  2
     public $J38_MaketImg; // макет изображения
-    public $J39_FilmFront; // пленка лицевая
-    public $J40_PlasticFront; // пластик лицевой
+    public $J39_PlenkLic; // пленка лицевая
+    public $J40_PlasticLic; // пластик лицевой
     public $J41_Light; // источник света
-    public $J42_Konstruktiv; // конструктив
 
     // шаг  3
 
     public function __construct($C_light = 0, $S_Light = 1,
-                                $RoofVisorOut = 0, $WallOut = 0, $WallIn = 0, $SideIn2 = 1, $SideIn4 = 0,
-                                $Orientacia = 1,
-                                $BigStor = 300, $SmallStor = 60,
-                                $FrontImg = 1, $ColorSide = 0, $ColorBack = 0,
-                                $MaketImg = 1, $FilmFront = 3, $PlasticFront = 1, $Light = 2,
-                                $Konstruktiv = 2)
+                                $VarIspoln = 4,
+                                $Orientacia = 1, $MaxSide = 150, $MinSide = 100, $LicIzobr = 1, $CvetBort =1, $CvetTil = 0,
+                                $LevVerhUgol = 0, $PravVerhUgol = 0, $PravNijnUgol = 0, $LevNijnUgol = 0,
+                                $MaketImg = 1, $PlenkLic = 3, $PlastLic = 2, $Light = 1
+                                )
 
     {
         // Заполнение входных данных.
         $this->B4_C_light = $C_light;
         $this->B5_S_Light = $S_Light;
-        // C_light
-        $this->J4_RoofVisorOut = $RoofVisorOut;
-        $this->J5_WallOut = $WallOut;
-        $this->J6_WallIn = $WallIn;
-        $this->J7_2SideIn = $SideIn2;
-        $this->J8_4SideIn = $SideIn4;
+        // C_light_2
+
+
+        // шаг 0
+        $this->J4_VarIspoln = $VarIspoln;
+
         // шаг  1
         $this->J21_Orientation = $Orientacia;
-        $this->J22_MaxSide_cm = $BigStor;
-        $this->J23_MinSide_cm = $SmallStor;
-        $this->J24_FrontImg = $FrontImg;
-        $this->J25_ColorSide = $ColorSide;
-        $this->J26_ColorBack = $ColorBack;
+        $this->J22_MaxSide_cm = $MaxSide;
+        $this->J23_MinSide_cm = $MinSide;
+        $this->J24_FrontImg = $LicIzobr;
+        $this->J25_ColorSide = $CvetBort;
+        $this->J26_ColorBack = $CvetTil;
+
+        $this->J29_LevVerhUgolRad = $LevVerhUgol;
+        $this->J30_PravVerhUgolRad = $PravVerhUgol;
+        $this->J31_PravNijnUgolRad = $PravNijnUgol;
+        $this->J32_LevNijnUgolRad = $LevNijnUgol;
+
         // шаг  2
         $this->J38_MaketImg = $MaketImg;
-        $this->J39_FilmFront = $FilmFront;
-        $this->J40_PlasticFront = $PlasticFront;
+        $this->J39_PlenkLic = $PlenkLic;
+        $this->J40_PlasticLic = $PlastLic;
         $this->J41_Light = $Light;
-        $this->J42_Konstruktiv = $Konstruktiv;
+
         // шаг  3
     }
     // S_light
