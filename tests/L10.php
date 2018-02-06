abeceeec70de497cff306db5973917eb40cd6294
Andrii <>
2018-02-06 23:24:34 +0200

    Andrii - Добавил тесты для tests/L10 Andrii - Исправил значения констант в src/L10

---
+++ tests/L10.php
@@ -0,0 +1,18 @@
+<?php namespace almaz44\light\calculator;
+/**
+ * Created by PhpStorm.
+ * User: andrii
+ * Date: 06.02.18
+ * Time: 23:13
+ */
+
+echo 'Ежедневные:';
+echo '<br>L10_C6_Dollar=' . L10_C6_Dollar;
+echo '<br>L10_C7_Euro=' . L10_C7_Euro;
+echo '<br>';
+echo '<br>L10_C9_BenzinL=' . L10_C9_BenzinL;
+echo '<br>L10_C10_Dizel=' . L10_C10_Dizel;
+echo '<br>L10_C11_Gaz=' . L10_C11_Gaz;
+echo '<br><br>Ежемесячные:';
+echo '<br>L10_C23_DvpWhite3mm=' . L10_C23_DvpWhite3mm;
+echo '<br>L10_C24_CatDvp=' . L10_C24_CatDvp;
