742e75f8c3c13286bc6dab813a902e9448881280
Andrii <>
2018-02-12 23:28:20 +0200

    Исправил тесты в L10.11

--- tests/L10_11-12.php
+++ tests/L10_11-12.php
@@ -11,78 +11,85 @@ echo '11. Коэфициенты, техно 1. 4-140(BA-BG)<hr>';
 echo 'L10_BB6_K_PererashodPVH=' . L10_BB6_K_PererashodPVH;
 echo '<br>L10_BB7_K_PererashodAkryl=' . L10_BB7_K_PererashodAkryl;
 echo '<br>L10_BB8_K_PererashodPolicarb=' . L10_BB8_K_PererashodPolicarb;
-echo '<br>L10_BB9_K_PererashodDVP=' . L10_BB9_K_PererashodDVP;
-
-echo '<br>L10_BB13_K_RashodSilNa1mpDT300ml=' . L10_BB13_K_RashodSilNa1mpDT300ml;
-echo '<br>L10_BB14_K_RashodCosmofenNa1mpDT200ml=' . L10_BB14_K_RashodCosmofenNa1mpDT200ml;
-
+echo '<br>L10_BB9_K_PererashodDVP' . L10_BB9_K_PererashodDVP;
+echo '<br>';
 echo '<br>L10_BB17_K_PVHPerehTolsh34Ulica_m=' . L10_BB17_K_PVHPerehTolsh34Ulica_m;
 echo '<br>L10_BB18_K_PVHPerehTolsh34Pomesh_m=' . L10_BB18_K_PVHPerehTolsh34Pomesh_m;
 echo '<br>L10_BB19_K_PVHPerehTolsh45KK_m=' . L10_BB19_K_PVHPerehTolsh45KK_m;
-
+echo '<br>';
 echo '<br>L10_BB24_K_AkrylPerehodTolsh23Ulica_m=' . L10_BB24_K_AkrylPerehodTolsh23Ulica_m;
 echo '<br>L10_BB25_K_AkrylPerehodTolsh23Pomesh_m=' . L10_BB25_K_AkrylPerehodTolsh23Pomesh_m;
-
-echo '<br>L10_BB29_K_PolicarbPerehTolsh46Ulica_m=' . L10_BB29_K_PolicarbPerehTolsh46Ulica_m;
+echo '<br>';
+echo '<br>L10_BB28_K_PolicarbPerehTolsh46UlicaB_m=' . L10_BB28_K_PolicarbPerehTolsh46UlicaB_m;
+echo '<br>L10_BB29_K_PolicarbPerehTolsh46UlicaM_m=' . L10_BB29_K_PolicarbPerehTolsh46UlicaM_m;
 echo '<br>L10_BB30_K_PolicarbPerehTolsh46Pomesh_m=' . L10_BB30_K_PolicarbPerehTolsh46Pomesh_m;
-
+echo '<br>';
 echo '<br>L10_BB34_K_GranicaPOLicUlica_m=' . L10_BB34_K_GranicaPOLicUlica_m;
 echo '<br>L10_BB35_K_GranicaPOLicPomesh_m=' . L10_BB35_K_GranicaPOLicPomesh_m;
-
+echo '<br>';
 echo '<br>L10_BB38_K_OpirPloshK_m2=' . L10_BB38_K_OpirPloshK_m2;
 echo '<br>L10_BB39_K_OpirPloshS_m2=' . L10_BB39_K_OpirPloshS_m2;
 echo '<br>L10_BB40_K_OpirPloshP_m2=' . L10_BB40_K_OpirPloshP_m2;
-
+echo '<br>';
 echo '<br>L10_BB45_K_PVH3mmMaxNagrNaPodv_kg=' . L10_BB45_K_PVH3mmMaxNagrNaPodv_kg;
 echo '<br>L10_BB46_K_PVH4mmMaxNagrNaPodv_kg=' . L10_BB46_K_PVH4mmMaxNagrNaPodv_kg;
 echo '<br>L10_BB47_K_PVH5mmMaxNagrNaPodv_kg=' . L10_BB47_K_PVH5mmMaxNagrNaPodv_kg;
 echo '<br>L10_BB48_K_DVP3mmMaxNagrNaPodv_kg=' . L10_BB48_K_DVP3mmMaxNagrNaPodv_kg;
 echo '<br>L10_BB49_K_GranPrimPodvOD_m=' . L10_BB49_K_GranPrimPodvOD_m;
-
+echo '<br>L10_BB50_K_DlnTrosDlVnutPodv_mp=' . L10_BB50_K_DlnTrosDlVnutPodv_mp;
+echo '<br>';
 echo '<br>L10_BB52_K_KolSticovTila1mp=' . L10_BB52_K_KolSticovTila1mp;
 echo '<br>L10_BB53_K_KolSticovBorta1mp=' . L10_BB53_K_KolSticovBorta1mp;
-
+echo '<br>';
 echo '<br>L10_BB56_K_PererashTrubaBlack_20x20_40x20=' . L10_BB56_K_PererashTrubaBlack_20x20_40x20;
 echo '<br>L10_BB57_K_PererashUgolAL_12x12_15x15=' . L10_BB57_K_PererashUgolAL_12x12_15x15;
 echo '<br>L10_BB58_K_PererashCDUD=' . L10_BB58_K_PererashCDUD;
 echo '<br>L10_BB58_K_PererashStilkoProf=' . L10_BB58_K_PererashStilkoProf;
 echo '<br>L10_BB60_K_KolSamorezVZadStShtMp=' . L10_BB60_K_KolSamorezVZadStShtMp;
 echo '<br>L10_BB61_K_KolSamorezVRamaPVHKorobShtMp=' . L10_BB61_K_KolSamorezVRamaPVHKorobShtMp;
-
+echo '<br>L10_BB62_K_KolSamorezVALRamaPVHKorobShtMp=' . L10_BB62_K_KolSamorezVALRamaPVHKorobShtMp;
+echo '<br>';
 echo '<br>L10_BB65_K_KoefPererashALDiodPolos=' . L10_BB65_K_KoefPererashALDiodPolos;
 echo '<br>L10_BB65_K_KoefPererashGibkDiodPolos=' . L10_BB65_K_KoefPererashGibkDiodPolos;
-
+echo '<br>';
 echo '<br>L10_BB68_K_KolSticovALDiodPolos1mp=' . L10_BB68_K_KolSticovALDiodPolos1mp;
 echo '<br>L10_BB69_K_KolSticovGibkDiodPolos1mp=' . L10_BB69_K_KolSticovGibkDiodPolos1mp;
-
+echo '<br>L10_BB70_K_PredkolKlast37503KrVSegmsht=' . L10_BB70_K_PredkolKlast37503KrVSegmsht;
+echo '<br>L10_BB71_K_PredDlnLentDiod3750144VtVSegmm=' . L10_BB71_K_PredDlnLentDiod3750144VtVSegmm;
 echo '<br>L10_BB72_K_LenghtOutputKabel_mp=' . L10_BB72_K_LenghtOutputKabel_mp;
 echo '<br>L10_BB73_K_ShagLinLamp1St_m=' . L10_BB73_K_ShagLinLamp1St_m;
+echo '<br>';
 echo '<br>L10_BB75_K_ShagLinALPolos_m=' . L10_BB75_K_ShagLinALPolos_m;
 echo '<br>L10_BB76_K_ShagLinGibkPolos_m=' . L10_BB76_K_ShagLinGibkPolos_m;
 echo '<br>L10_BB77_PloshOsvKlast3750and3krandIP65_m2=' . L10_BB77_PloshOsvKlast3750and3krandIP65_m2;
-
 echo '<br>L10_BB78_K_ZapasPoTokuDlBP=' . L10_BB78_K_ZapasPoTokuDlBP;
-
-echo '<br>L10_BB81_K_PredelTokMed1mm2A=' . L10_BB81_K_PredelTokMed1mm2A;
-echo '<br>L10_BB82_K_PredelTokMed15mm2A=' . L10_BB82_K_PredelTokMed15mm2A;
-
+echo '<br>L10_BB79_K_GarantFinansNaBP=' . L10_BB79_K_GarantFinansNaBP;
+echo '<br>L10_BB80_K_DlnLampMontProvodPV1=' . L10_BB80_K_DlnLampMontProvodPV1;
+echo '<br>';
 echo '<br>L10_BB85_K_PererashPlenkFasad=' . L10_BB85_K_PererashPlenkFasad;
 echo '<br>L10_BB86_K_PererashPolnocvet=' . L10_BB86_K_PererashPolnocvet;
 echo '<br>L10_BB87_K_PererashPlenkBort=' . L10_BB87_K_PererashPlenkBort;
 echo '<br>L10_BB88_K_PererashPlenkTil=' . L10_BB88_K_PererashPlenkTil;
-
+echo '<br>';
 echo '<br>L10_BB93_K_PloshFasadPloshPlenkDlApp=' . L10_BB93_K_PloshFasadPloshPlenkDlApp;
 echo '<br>L10_BB94_K_PloshFasadPloshPlenkTransp=' . L10_BB94_K_PloshFasadPloshPlenkTransp;
 echo '<br>L10_BB95_K_PerimVivesDlinaResaPlot=' . L10_BB95_K_PerimVivesDlinaResaPlot;
-
+echo '<br>';
+echo '<br>L10_BB100_K_RashSkotchStreych=' . L10_BB100_K_RashSkotchStreych;
 echo '<br>L10_BB101_K_PererashStrchObshPlVives=' . L10_BB101_K_PererashStrchObshPlVives;
 echo '<br>L10_BB102_K_PererashKartObshPlVives=' . L10_BB102_K_PererashKartObshPlVives;
 echo '<br>L10_BB103_K_PererashUgolKartPerimVives=' . L10_BB103_K_PererashUgolKartPerimVives;
-
+echo '<br>L10_BB104_K_ShagStyajDerPlanoksht1mpPerimFasad=' . L10_BB104_K_ShagStyajDerPlanoksht1mpPerimFasad;
+echo '<br>L10_BB105_K_PererashDerPlanokupak=' . L10_BB105_K_PererashDerPlanokupak;
+echo '<br>';
 echo '<br>L10_BB109_K_PloshStandartVives=' . L10_BB109_K_PloshStandartVives;
 echo '<br>L10_BB110_K_PopravPloshSnabj=' . L10_BB110_K_PopravPloshSnabj;
-
-echo '<br>L10_BB114_K_ProbAvtSnabj100kmVivesStandart=' . L10_BB114_K_ProbAvtSnabj100kmVivesStandart;
+echo '<br>L10_BB111_K_KolPoezdSnabjBezPlankStandartsht=' . L10_BB111_K_KolPoezdSnabjBezPlankStandartsht;
+echo '<br>L10_BB112_K_KolPoezdSnabjPlenkStandartsht=' . L10_BB112_K_KolPoezdSnabjPlenkStandartsht;
+echo '<br>L10_BB113_K_ProbegDobloZa1PoezdSnabj100km=' . L10_BB113_K_ProbegDobloZa1PoezdSnabj100km;
+echo '<br>';
+echo '<br>L10_BB115_K_ZatratnaDostListMatlDis=' . L10_BB115_K_ZatratnaDostListMatlDis;
+echo '<br>L10_BB116_K_TransportRashNaVivNachSoVt=' . L10_BB116_K_TransportRashNaVivNachSoVt;
 
 // =====================================================
 echo '<br>';
