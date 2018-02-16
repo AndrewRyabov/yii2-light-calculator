835d2ba60c72f86d067331d5b43b9e461cd06338
Andrii <>
2018-02-16 09:53:53 +0200

    Тест L15 косметические изменения.

--- tests/L15_Q.php
+++ tests/L15_Q.php
@@ -6,7 +6,7 @@
  * Time: 18:52
  */
 
-echo '<b>Входные параметры (фасад пленка затяжка 1):</b>';
+echo '<b>Входные параметры:</b>';
 echo '<br>крыша/козырек улица =' . $L15_2->R5_RoofVisorOut;
 echo '<br>стена улица =' . $L15_2->R6_WallOut;
 echo '<br>стена помещение =' . $L15_2->R7_WallIn;
@@ -23,14 +23,13 @@ echo '<br><b>ширина пленки 1 м</b>';
 echo '<hr>1 сторона итого, грн: =' . $L15_2->Z31_StoronaItogo_grn();
 echo '<br>2 сторона итого, грн: =' . $L15_2->AD31_StoroniItogo_grn();
 echo '<br>4 сторона итого, грн: =' . $L15_2->AH31_StoroniItogo_grn();
-echo '<br>грн: =' . $L15_2->AJ31_Itogo1m_grn();
+echo '<br>===========грн: =' . $L15_2->AJ31_Itogo1m_grn();
 echo '<br>';
 echo '<br><b>ширина пленки 1.22 м</b>';
 echo '<hr>1 сторона итого, грн: =' . $L15_2->Z48_StoronaItogo_grn();
 echo '<br>2 сторона итого, грн: =' . $L15_2->AD48_StoroniItogo_grn();
 echo '<br>4 сторона итого, грн: =' . $L15_2->AH48_StoroniItogo_grn();
-echo '<br>грн: =' . $L15_2->AJ48_Itogo1Tochka22m_grn();
-echo '<br><b>итого пленка + порезка + переасход</b>, грн: =' . $L15_2->AJ50_ItogoPlenkaPlusPorezkaPlusPerarasxod();
+echo '<br>===========грн: =' . $L15_2->AJ48_Itogo1Tochka22m_grn();
 
 echo '<hr><b>Выходные параметры:</b>';
 echo '<br>итого, грн. =' . $L15_2->AM24_Itogo_grn();
