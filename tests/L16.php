31ebcb3b80131638ccb733500065b753eb5940dc
Andrii <>
2018-02-16 09:54:19 +0200

    Тест L16 - заготовка.

---
+++ tests/L16.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrii
+ * Date: 16.02.18
+ * Time: 9:43
+ */
+?>
+<table border="1">
+    <tr>
+        <td colspan="3" align="center">Class 16</td>
+    </tr>
+    <tr>
+        <td>Фасад пластик</td>
+        <td>Борт ПВХ</td>
+        <td>Тыл ПВХ/ДВП</td>
+    </tr>
+    <tr>
+        <td><?php //include_once 'L16_A.php' ?></td>
+        <td><?php //include_once 'L16_R.php' ?></td>
+        <td><?php //include_once 'L16_AI.php'?></td>
+</tr>
+</table>
--- tests/index.php
+++ tests/index.php
@@ -87,24 +87,10 @@ $L25 = new L25();
 <!------------------------------------------------------------------------->
 <br><br>
 <?php include_once 'L15.php' ?>
-<?php return(0) ?>
 <!------------------------------------------------------------------------->
 <br><br>
-<table border="1">
-    <tr>
-        <td colspan="3" align="center">Class 16</td>
-    </tr>
-    <tr>
-        <td>Фасад пластик</td>
-        <td>Борт ПВХ</td>
-        <td>Тыл ПВХ/ДВП</td>
-    </tr>
-    <tr>
-        <td><?php include_once 'L16_A.php' ?></td>
-        <td><?php include_once 'L16_R.php' ?></td>
-        <td><?php include_once 'L16_AI.php'?></td>
-    </tr>
-</table>
+<?php include_once 'L16.php' ?>
+<?php return(0) ?>
 <!------------------------------------------------------------------------->
 <br><br>
 <table border="1">
