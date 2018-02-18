922b7f471e31262caf0f8b8b966e31aa135c7919
Andrii <>
2018-02-18 15:24:11 +0200

    заготовки для тестов L16 и L17.

---
+++ tests/L17.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrii
+ * Date: 16.02.18
+ * Time: 9:43
+ */
+?>
+<table border="1">
+    <tr>
+        <td colspan="3" align="center">Class 17</td>
+    </tr>
+    <tr>
+        <td>Опоры лицевого пластика</td>
+        <td>рама внутренняя</td>
+        <td>рама внешняя 4 стороны</td>
+        <td>рама электро</td>
+    </tr>
+    <tr>
+        <td><?php //include_once 'L17_A.php' ?></td>
+        <td><?php //include_once 'L17_.php' ?></td>
+        <td><?php //include_once 'L17_.php'?></td>
+        <td><?php //include_once 'L17_.php'?></td>
+</tr>
+</table>
