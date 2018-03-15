39b440381c6cea217358041c9e0d97a29af90af9
Andrii <>
2018-03-15 17:12:12 +0200

    Заготовка для теста L18

---
+++ tests/L18.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrii
+ * Date: 07.03.18
+ * Time: 0:09
+ */
+?>
+<table border="1">
+    <tr>
+        <td colspan="4" align="center">Class 18</td>
+    </tr>
+    <tr>
+        <td>C-light _ лампы дневного света</td>
+        <td>C-light _ кластеры 3750*3*1,3 внешний БП</td>
+        <td>C-light _ лента диодная 4,6 Вт "Алексей" внешний БП</td>
+        <td>C-light _ электрика итого</td>
+    </tr>
+    <tr>
+        <td><?php include_once 'L18_A.php' ?></td>
+        <td><?php include_once 'L18_S.php' ?></td>
+        <td><?php include_once 'L18_AK.php'?></td>
+        <td><?php include_once 'L18_BC.php'?></td>
+</tr>
+</table>
---
+++ tests/L18_A.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrii
+ * Date: 15.03.18
+ * Time: 17:05
+ */
+
+echo '<b>Входные параметры:</b>';
+echo '<br>крыша/козырек улица =' . $L17_2->S5_RoofVisorOut;
+echo '<br>стена улица =' . $L17_2->S6_WallOut;
+echo '<br>стена помещение =' . $L17_2->S7_WallIn;
+echo '<br>2 стороны помещение =' . $L17_2->S8_2SideIn;
+echo '<br>4 стороны помещение =' . $L17_2->S9_4SideIn;
+echo '<br>';
+echo '<br>';
+echo '<br>стоимость опор лицевых, грн =' . $L17_2->S20_CosSupportFront;
