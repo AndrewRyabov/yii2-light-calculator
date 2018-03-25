2416d017938d69ee3b6ab44f8f761d0d31c7dc73
Andrii <>
2018-03-25 18:25:01 +0300

    Andrii-Заготовки для L19

---
+++ tests/L19_A.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrii
+ * Date: 15.03.18
+ * Time: 17:05
+ */
+
+echo '<b>Входные_параметры:</b>';
+//echo '<br>Большая сторона =' . $L18_1->B5_BigSide_cm;
+//echo '<br>Меньшая сторона =' . $L18_1->B6_SmallSide_cm;
+//
+echo '<hr><b>Выходная величина:</b>';
+//echo '<br>Стоимость_материалов,_грн=' . $L18_1->P6_StoimMatgrn();
+//echo '<br>';
+//echo '<br>Трудоёмкость ламп, мин =' . $L18_1->P10_TrudLampmin();
+//echo '<br>Стоимость работы, грн =' . $L18_1->P11_StoimRabgrn();
+//
+//echo '<hr><b>Энергопотребление, вт =' . $L18_1->P21_Energopotvt() . '</b>';
+//echo '<br><b>Вес, кг =' . $L18_1->P22_Veskg() . '</b>';
+//echo '<br><b>Итого, грн =' . $L18_1->P24_Itogogrn() . '</b>';
