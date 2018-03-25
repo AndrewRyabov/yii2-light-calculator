9d84dae22fa2047b29314f4b59133868ea90705a
Andrii <>
2018-03-25 22:01:49 +0300

    Корректировка тестов L09

--- tests/L09_0.php
+++ tests/L09_0.php
@@ -6,4 +6,15 @@
  * Time: 2:24
  */
 
-echo 'J4_J4_VarIspoln=' . $L09->J4_VarIspoln;
+switch ($L09->J4_VarIspoln) {
+    case 1: $temp = 'крыша'; break;
+    case 2: $temp = 'улица'; break;
+    case 3: $temp = 'помещ'; break;
+    case 4: $temp = '2 стор'; break;
+    case 5: $temp = '4 стор'; break;
+    default: $temp = 'ОШИБКА';
+}
+
+echo 'Вариант исполнения:';
+echo '<br><br>';
+echo 'J4_J4_VarIspoln =' . $L09->J4_VarIspoln .' ('.$temp.')';
--- tests/L09_2.php
+++ tests/L09_2.php
@@ -9,4 +9,12 @@
 echo 'J38_MaketImg=' . $L09->J38_MaketImg;
 echo '<br>J39_PlenkLic=' . $L09->J39_PlenkLic;
 echo '<br>J40_PlasticLic=' . $L09->J40_PlasticLic;
-echo '<br>J41_Light=' . $L09->J41_Light;
+
+// источник света (1-диоды внеш / 2-диоды встроен / 3-лампы)
+switch ($L09->J41_Light) {
+    case 1: $temp = 'диоды внешние'; break;
+    case 2: $temp = 'диоды встроеннные'; break;
+    case 3: $temp = 'лампы'; break;
+    default: $temp = 'ОШИБКА';
+}
+echo '<br>J41_Light=' . $L09->J41_Light .' ('.$temp.')';
