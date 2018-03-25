fb3c32edb5439c384c7518f07bb7feb0485d7fa4
Andrii <>
2018-03-25 18:25:14 +0300

    Собрал в кучу L18

---
+++ tests/L25.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrii
+ * Date: 07.03.18
+ * Time: 0:09
+ */
+?>
+<table border="1">
+    <tr>
+        <td colspan="7" align="center">Class 25</td>
+    </tr>
+    <tr>
+        <td>C-light интегратор лайтбокс</td>
+    </tr>
+    <tr>
+        <td><?php include_once 'L25_A.php'?></td>
+</tr>
+</table>
