1c8dfc6263b808b9aca054d4c78daef1e59c36c9
Andrii <>
2018-03-25 18:25:01 +0300

    Andrii-Заготовки для L19

---
+++ tests/L19.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrii
+ * Date: 07.03.18
+ * Time: 0:09
+ */
+?>
+<table border="1">
+    <tr>
+        <td colspan="7" align="center">Class 19</td>
+    </tr>
+    <tr>
+        <td>C-light снабжение 1</td>
+        <td>C-light подвесы 1</td>
+    </tr>
+    <tr>
+        <td><?php include_once 'L19_A.php'?></td>
+        <td><?php include_once 'L19_Q.php'?></td>
+</tr>
+</table>
