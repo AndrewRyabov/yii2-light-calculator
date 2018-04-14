f013796b901f5b738cdc904ecf6a6a03ad3e1b00
AndrewRyabov <>
2018-04-14 20:10:56 +0300

    L25

--- tests/L25_Itogo.php
+++ tests/L25_Itogo.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Andrii
+ * User: Andrew
  * Date: 07.06.2017
  * Time: 2:24
  */
-
-//echo 'O30_StoimIzgot1shtgrn=' . $L25->O30_StoimIzgot1shtgrn();
-//echo '<br>O31_Energopotrebleniyevt=' . $L25->O31_Energopotrebleniyevt();
-//echo '<br>O32_Veskg=' . $L25->O32_Veskg();
+// Вывод переменных:
+echo 'O30_StoimIzgot1shtgrn=' . $L25->O30_StoimIzgot1shtgrn();
+echo '<br>O31_Energopotrebleniyevt=' . $L25->O31_Energopotrebleniyevt();
+echo '<br>O32_Veskg=' . $L25->O32_Veskg();
