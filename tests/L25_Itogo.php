e5c14af2331990d037b38fc92af50d994f793fce
Andrii <>
2018-04-15 17:18:57 +0300

    TEST добавил итоговую информацию из L25 в шапку таблицы L09

--- tests/L25_Itogo.php
+++ tests/L25_Itogo.php
@@ -6,6 +6,6 @@
  * Time: 2:24
  */
 // Вывод переменных:
-echo 'O30_StoimIzgot1shtgrn=' . $L25->O30_StoimIzgot1shtgrn();
-echo '<br>O31_Energopotrebleniyevt=' . $L25->O31_Energopotrebleniyevt();
-echo '<br>O32_Veskg=' . $L25->O32_Veskg();
+echo 'Стоимость (нал.), грн =' . $L25->O29_StoimostBazovya_grn();
+echo '<br>Энергопотребление, Вт =' . $L25->O31_Energopotrebleniyevt();
+echo '<br>Вес, кг =' . $L25->O32_Veskg();
